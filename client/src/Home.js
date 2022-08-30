33975a4b1a98f205b60a4230e7f7f503a20ff84a
alexander gubernikoff <agubernikoff@gmail.com>
2022-08-30 13:03:33 -0400

    clapperboard buttons

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React,{useRef} from "react";
 import mills_intro from "./Backdrop.jpeg";
 
 function Home() {
+  const top=useRef(null)
+  const top2=useRef(null)
+
+  function open(ref){ref.current.className="clapper-open"}
+  function close(ref){ref.current.className='clapper-close'}
   return (
     <div className="home">
       <h1 className="max_head">MAX MILLSTEIN</h1>
       <img src={mills_intro} />
       <div className="button_hold">
-        <button>something</button>
-        <button>something else</button>
+        <div class='clapper'>
+          <div ref={top}></div>
+          <div></div>
+          <button onMouseEnter={()=>open(top)} onMouseLeave={()=>close(top)}>something</button>
+        </div>
+        <div class='clapper'>
+          <div ref={top2}></div>
+          <div></div>
+          <button onMouseEnter={()=>open(top2)} onMouseLeave={()=>close(top2)}>something else</button>
+        </div>
       </div>
     </div>
   );
