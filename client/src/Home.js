12c600681e3b87e480fe7b7ae5cae01c75595c2c
alexander gubernikoff <agubernikoff@gmail.com>
2022-08-30 17:51:50 -0400

    updated animation/hover

--- client/src/Home.js
+++ client/src/Home.js
@@ -12,15 +12,15 @@ function Home() {
       <h1 className="max_head">MAX MILLSTEIN</h1>
       <img src={mills_intro} />
       <div className="button_hold">
-        <div class='clapper'>
+        <div onMouseEnter={()=>open(top)} onMouseLeave={()=>close(top)} class='clapper'>
           <div ref={top}></div>
           <div></div>
-          <button onMouseEnter={()=>open(top)} onMouseLeave={()=>close(top)}>something</button>
+          <button>something</button>
         </div>
-        <div class='clapper'>
+        <div onMouseEnter={()=>open(top2)} onMouseLeave={()=>close(top2)} class='clapper'>
           <div ref={top2}></div>
           <div></div>
-          <button onMouseEnter={()=>open(top2)} onMouseLeave={()=>close(top2)}>something else</button>
+          <button>something else</button>
         </div>
       </div>
     </div>
