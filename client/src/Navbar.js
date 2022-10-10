d37c238526d8c1d48b379380ff99eaf5183477f4
alexander gubernikoff <agubernikoff@gmail.com>
2022-10-10 19:11:48 -0400

    active style

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -2,17 +2,31 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  const activeStyle = ({ isActive }) =>
+    isActive
+      ? {
+          color: "grey",
+        }
+      : null;
   return (
     <div id="navbar">
       <div className="topnav" id="myTopnav">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About / Contact</NavLink>
+        <NavLink style={activeStyle} to="/">
+          Home
+        </NavLink>
+        <NavLink style={activeStyle} to="/about">
+          About / Contact
+        </NavLink>
         <div>
           <h1 className="max_nav_bar">MAX MILLSTEIN</h1>
           <h3>WRITER · DIRECTOR</h3>
         </div>
-        <NavLink to="/red_hook">Red Hook</NavLink>
-        <NavLink to="/glacial_ways">Glacial Ways</NavLink>
+        <NavLink style={activeStyle} to="/red_hook">
+          Red Hook
+        </NavLink>
+        <NavLink style={activeStyle} to="/glacial_ways">
+          Glacial Ways
+        </NavLink>
         {/* <NavLink onClick={myFunction} to="/glacial_ways">
           Glacial Ways
         </NavLink> */}
