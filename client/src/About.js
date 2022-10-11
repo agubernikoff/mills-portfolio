1a0238222ac45debe2f421a46701863497f9b348
alexander gubernikoff <agubernikoff@gmail.com>
2022-10-10 23:42:24 -0400

    mobile layout for about

--- client/src/About.js
+++ client/src/About.js
@@ -6,29 +6,25 @@ function About() {
   return (
     <div>
       <div className="aboutpage">
-        <h2>ABOUT / CONTACT</h2>
-        <div className="left_side">
-          <img className="biopic" src={biopic} />
-        </div>
-        <div className="right_side">
-          <div className="max_text">
-            <h3>MEET MAX</h3>
-            <p>
-              Max Millstein is writer/director living in Brooklyn, NY. His
-              directorial debut short film titled Red Hook hit the festival
-              circuit in October 2022. He is currently in post-production with
-              his second short film -- a whimsical and evocative climate change
-              awareness piece. Max primarily writes drama/thriller features.
-              <br></br>
-              <br></br>A data analyst consultant during the day, Max is no
-              stranger to creating budgets and managing projects with fast
-              deadlines. Understanding the practicality and collaborative nature
-              of film, Max is able to bring his grandiose visions into fruition.
-            </p>
+        <h1>ABOUT / CONTACT</h1>
+        <img className="biopic" src={biopic} alt="max millstein" />
+        <div className="max_text">
+          <h1>MEET MAX</h1>
+          <p>
+            Max Millstein is writer/director living in Brooklyn, NY. His
+            directorial debut short film titled Red Hook hit the festival
+            circuit in October 2022. He is currently in post-production with his
+            second short film -- a whimsical and evocative climate change
+            awareness piece. Max primarily writes drama/thriller features.
+            <br></br>
+            <br></br>A data analyst consultant during the day, Max is no
+            stranger to creating budgets and managing projects with fast
+            deadlines. Understanding the practicality and collaborative nature
+            of film, Max is able to bring his grandiose visions into fruition.
+          </p>
 
-            <h3>LETS WORK TOGETHER</h3>
-            <p>Email: maxmillstein1@gmail.com</p>
-          </div>
+          <h1>LETS WORK TOGETHER</h1>
+          <p>maxmillstein1@gmail.com</p>
         </div>
       </div>
       {/* <Footer /> */}
