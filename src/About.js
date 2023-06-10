b63a2690d111cd47948c91ebdfd5b3ebeddceb6a
alexander gubernikoff <agubernikoff@gmail.com>
2023-06-10 00:35:12 -0400

    bio

--- src/About.js
+++ src/About.js
@@ -17,17 +17,18 @@ function About() {
           <div className="max_text">
             <h1>MEET MAX</h1>
             <p>
-              Max Millstein is an award winning writer/director living in
-              Brooklyn, NY. His directorial debut short film titled Red Hook hit
-              the festival circuit in October 2022. He is currently in
-              post-production with his second short film -- a whimsical and
-              evocative climate change awareness story. Max primarily writes
-              drama/thriller features.
+              Max Millstein is an award winning writer/director living in NYC.
+              His directorial debut short film titled Red Hook has screened at
+              over a dozen festivals including the Culver City Film Festival and
+              the Big Apple Film Festival. He is currently in post-production
+              with his second short film. Max primarily writes drama/thriller
+              features.
               <br></br>
               <br></br>A data analyst consultant during the day, Max is no
               stranger to creating budgets and managing projects with fast
               deadlines. Understanding the practicality and collaborative nature
-              of film, Max is able to bring his grandiose visions into fruition.
+              of film, Max is able to produce and bring his grandiose visions
+              into fruition.
             </p>
             <h1>LET'S WORK TOGETHER</h1>
 
